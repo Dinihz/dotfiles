554ff94bd7213261458ae6de2c641de75854172e
dinihz <lucas87diniz@gmail.com>
2025-03-07 18:58:58 -0300

    feat: add my i3 and i3status config

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -10,25 +10,25 @@ if true then return {} end
 -- * override the configuration of LazyVim plugins
 return {
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
+  -- { "ellisonleao/gruvbox.nvim" },
 
   -- Configure LazyVim to load gruvbox
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
-    },
+    -- "LazyVim/LazyVim",
+    -- opts = {
+    --   colorscheme = "gruvbox",
+    -- },
   },
 
   -- change trouble config
-  {
-    "folke/trouble.nvim",
-    -- opts will be merged with the parent spec
-    opts = { use_diagnostic_signs = true },
-  },
-
+  -- {
+  --   "folke/trouble.nvim",
+  --   -- opts will be merged with the parent spec
+  --   opts = { use_diagnostic_signs = true },
+  -- },
+  --
   -- disable trouble
-  { "folke/trouble.nvim", enabled = false },
+  -- { "folke/trouble.nvim", enabled = false },
 
   -- override nvim-cmp and add cmp-emoji
   {
